7c032c7903a1d6e6118e2d3be983e8f7c9ee3529
Marllock <marcellovbenites@hotmail.com>
2023-05-20 02:18:02 -0300

    feat: adjust sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { HistoricModule } from './historic/historic.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'Stock',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductModule,
+    HistoricModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })

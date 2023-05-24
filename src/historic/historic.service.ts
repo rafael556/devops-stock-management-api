48cddc6461f0251e456339651599ad0de9e63172
rafael556 <rafael7jf@hotmail.com>
2023-05-24 20:06:30 -0300

    fix: adjust find all historic with product

--- src/historic/historic.service.ts
+++ src/historic/historic.service.ts
@@ -4,6 +4,7 @@ import { Historic } from './entities/historic.entity';
 import { Repository } from 'typeorm';
 import { Product } from 'src/product/entities/product.entity';
 import { HistoricStatusEnum } from './constants/historicStatus.enum';
+import { HistoricDto } from './dto/historicDto.dto';
 
 @Injectable()
 export class HistoricService {
@@ -13,7 +14,23 @@ export class HistoricService {
   ) {}
 
   async findAll() {
-    return await this.historicRepository.find();
+    const historic: Historic[] = await this.historicRepository
+      .createQueryBuilder('historic')
+      .leftJoinAndSelect('historic.historicProduct', 'product')
+      .getMany();
+
+    const historicDtoList: HistoricDto[] = [];
+    historic.forEach((e) => {
+      const historicDto: HistoricDto = {
+        historicId: e.historicId,
+        historicCreatedAt: e.historicCreatedAt,
+        historicStatus: e.historicStatus,
+        ProductAmount: e.historicProductAmount,
+        productName: e.historicProduct.productName,
+      };
+      historicDtoList.push(historicDto);
+    });
+    return historicDtoList;
   }
 
   async create(newProduct: Product, historicStatus: HistoricStatusEnum) {
--- src/historic/test/historic.mock.ts
+++ src/historic/test/historic.mock.ts
@@ -1,10 +1,10 @@
-import { productMock } from "../../product/test/product.mock";
-import { Historic } from "../entities/historic.entity";
+import { productMock } from '../../product/test/product.mock';
+import { Historic } from '../entities/historic.entity';
 
 export const historicMock: Historic = {
-    historicId: 1,
-    historicCreatedAt: "2023-01-01",
-    historicStatus: "CREATED",
-    historicProductAmount: 10,
-    historicProduct: productMock
-}
+  historicId: 1,
+  historicCreatedAt: '2023-01-01',
+  historicStatus: 'CREATED',
+  historicProductAmount: 10,
+  historicProduct: productMock,
+};
